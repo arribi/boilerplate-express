e49585d77ddd6d404c344c9c7d2e7d9c6d24d72b
Jesús Arribi <arribi@boxer.local>
2023-12-21 20:29:50 +0000

    Implement a Root-Level Request Logger Middleware

--- myApp.js
+++ myApp.js
@@ -7,8 +7,14 @@ let app = express();
 // app.get('/', function (req, res) {
 //   res.send('Hello Express');
 // });
+app.use(function (req, res, next) {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+});
 
 app.use("/public", express.static(__dirname + "/public"));
+
+
 
 app.get('/', (req, res) => {
   res.sendFile('views/index.html', { root: __dirname });
@@ -22,6 +28,8 @@ app.get('/json', (req, res) => {
   }
   res.json({ "message": response });
 });
+
+
 
 
 
