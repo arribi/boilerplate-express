bdfe563840b132213361abd4754c881de892ac65
Jesús Arribi <arribi@boxer.local>
2023-12-21 10:37:18 +0000

    Serve Static Assets

--- myApp.js
+++ myApp.js
@@ -7,6 +7,8 @@ let app = express();
 //   res.send('Hello Express');
 // });
 
+app.use("/public", express.static(__dirname + "/public"));
+
 app.get('/', (req, res) => {
   res.sendFile('views/index.html', { root: __dirname });
 });
