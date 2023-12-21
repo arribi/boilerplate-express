89e10e8479cd01b56a26ac981a29e246f12875ca
Jesús Arribi <arribi@boxer.local>
2023-12-21 09:49:56 +0000

    Send HTML file

--- myApp.js
+++ myApp.js
@@ -3,9 +3,14 @@ let app = express();
 
 // console.log("Hello World");
 
-app.get('/', function (req, res) {
-  res.send('Hello Express');
+// app.get('/', function (req, res) {
+//   res.send('Hello Express');
+// });
+
+app.get('/', (req, res) => {
+  res.sendFile('views/index.html', { root: __dirname });
 });
+
 
 
 
